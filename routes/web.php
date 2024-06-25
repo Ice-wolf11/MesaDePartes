6e7c760ddeef2f1f59a7dd76a550239564be3347
Andrew <andrewsegundo652@gmail.com>
2024-06-24 23:12:18 -0500

    Plantilla importada

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,22 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('template');
+});
+
+//login
+Route::get('/login', function () {
+    return view('auth.login');
+});
+
+
+//errores
+Route::get('/401', function () {
+    return view('pages.401');
+});
+Route::get('/404', function () {
+    return view('pages.404');
+});
+Route::get('/500', function () {
+    return view('pages.500');
 });
