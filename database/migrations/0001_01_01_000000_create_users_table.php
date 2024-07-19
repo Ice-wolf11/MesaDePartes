9f6c58777b87dca26d68173e79d7d7cacbd87fef
Andrew <andrewsegundo652@gmail.com>
2024-07-19 00:42:50 -0500

    Corrigiendo unas migraciones

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at');
+            $table->timestamp('email_verified_at')->nullable()->change();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
@@ -35,6 +35,7 @@ return new class extends Migration
             $table->longText('payload');
             $table->integer('last_activity')->index();
         });
+
     }
 
     /**
